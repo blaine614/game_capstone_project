7ce012858c533bcc02e622882b0832c62c5ad516
Aaron Ebbinghaus <aaronebb@gmail.com>
2015-09-08 14:57:41 -0400

    Hopefull this works, This is the pickup and drop object logic

---
+++ Assets/_Scripts/LockCursor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockCursor : MonoBehaviour {
+
+	void Start ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = (false);
+	}
+	void Update ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = (false);
+	}
+}
