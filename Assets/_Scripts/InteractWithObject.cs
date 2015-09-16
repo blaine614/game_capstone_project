dc92ee56ddbbb4cb58d1b0fac88f38d26ed97696
MadMagyar92 <joseph.bota@gmail.com>
2015-09-16 15:57:35 -0400

    Added interactable interface, keypad scripts, and a keypad in the puzzleunity test. Also added some sounds.

---
+++ Assets/_Scripts/InteractWithObject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractWithObject : MonoBehaviour {
+	GameObject mainCamera;
+	public float distance;
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			interact ();
+		}
+	}
+	
+	void interact() {
+		int x = Screen.width / 2;
+		int y = Screen.height / 2;
+			
+		Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x,y));
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit)) {
+			Interactable i = hit.collider.GetComponent<Interactable>();
+			if(i != null) {
+				i.Action();
+			}
+		}
+	}
+}
+
---
+++ Assets/_Scripts/KeyPadInteractable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPadInteractable : MonoBehaviour, Interactable {
+
+	public AudioClip keyClip;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		SetupAudioSource ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Action () {
+		audioSource.Play ();
+	}
+
+	void SetupAudioSource() {
+		audioSource = gameObject.AddComponent<AudioSource>();
+		audioSource.clip = keyClip;
+		audioSource.volume = 0.5f;
+		audioSource.dopplerLevel = 0.0f;
+		audioSource.loop = false;
+		audioSource.maxDistance = 1.5f;
+		audioSource.minDistance = 0.5f;
+		audioSource.spatialBlend = 1.0f;
+	}
+}
