3aa333cbc828fbf94e46f272891c8e989245ad78
Aaron Ebbinghaus <aaronebb@gmail.com>
2015-09-20 22:00:07 -0400

    Light stuff

---
+++ Assets/_Scripts/Flicker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour {
+
+	public Light flashingLight;
+	// public Light secondFlashingLight;
+	private float randomNumber;
+	
+	void Start(){
+		
+		flashingLight.enabled = false;
+		//secondFlashingLight.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		randomNumber = Random.value;
+		
+		if (randomNumber <= 0.95f) {
+			
+			flashingLight.enabled = true;
+			// secondFlashingLight.enabled = true;
+		} else {
+			
+			flashingLight.enabled = false;
+			//  secondFlashingLight.enabled = false;
+			
+		}
+		
+
+ }
+}
