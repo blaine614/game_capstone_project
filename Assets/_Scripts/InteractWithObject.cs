09434030d9fada97e567ae5da29e124659f2d4dc
MadMagyar92 <joseph.bota@gmail.com>
2015-10-01 12:05:29 -0400

    Making notes work. We can discuss later.

--- Assets/_Scripts/InteractWithObject.cs
+++ Assets/_Scripts/InteractWithObject.cs
@@ -1,26 +1,33 @@
 ﻿using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 using System.Collections;
+using UnityEngine.UI;
 
 public class InteractWithObject : MonoBehaviour {
 	GameObject mainCamera;
+	GameObject canvas;
 	public float distance;
 
 	private bool readingNote;
 	// Use this for initialization
 	void Start () {
 		mainCamera = GameObject.FindWithTag("MainCamera");
+		canvas = GameObject.FindWithTag ("Canvas");
 		readingNote = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.E)) {
-			interact ();
+			if(readingNote) {
+				StopNote ();
+			} else {
+				Interact ();
+			}
 		}
 	}
 	
-	void interact() {
+	void Interact() {
 		int x = Screen.width / 2;
 		int y = Screen.height / 2;
 			
@@ -37,16 +44,26 @@ public class InteractWithObject : MonoBehaviour {
                 p.SetActive(false);
                 gameObject.GetComponent<SanityMeter>().IncreaseSanity(50.0f);
             } else if (p.CompareTag("Note")){
-				ReadNote ();
+				ReadNote ("Some note.");
 			}
 		}
 	}
 
-	void ReadNote() {
+	void ReadNote(string note) {
+		readingNote = true;
+		canvas.GetComponent<Canvas>().enabled = true;
+		canvas.GetComponent<Text> ().text = note;
+		gameObject.GetComponent<RigidbodyFirstPersonController> ().mouseLook.XSensitivity = 0.0f;
+		gameObject.GetComponent<RigidbodyFirstPersonController> ().mouseLook.YSensitivity = 0.0f;
 		gameObject.GetComponent<RigidbodyFirstPersonController> ().movementSettings.ForwardSpeed = 0.0f;
 		gameObject.GetComponent<RigidbodyFirstPersonController> ().movementSettings.BackwardSpeed = 0.0f;
 		gameObject.GetComponent<RigidbodyFirstPersonController> ().movementSettings.StrafeSpeed = 0.0f;
+	}
 
+	void StopNote() {
+		canvas.GetComponent<Canvas>().enabled = false;
+		gameObject.GetComponent<RigidbodyFirstPersonController> ().mouseLook.XSensitivity = 2.0f;
+		gameObject.GetComponent<RigidbodyFirstPersonController> ().mouseLook.YSensitivity = 2.0f;
 		gameObject.GetComponent<RigidbodyFirstPersonController> ().movementSettings.ForwardSpeed = 6.0f;
 		gameObject.GetComponent<RigidbodyFirstPersonController> ().movementSettings.BackwardSpeed = 3.0f;
 		gameObject.GetComponent<RigidbodyFirstPersonController> ().movementSettings.StrafeSpeed = 3.0f;
