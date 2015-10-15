ac2783b6c902359609d872ae650018470530e340
Aaron Ebbinghaus <aaronebb@gmail.com>
2015-10-15 16:23:12 -0400

    Messing with DA Lights

--- Assets/_Scripts/Flicker.cs
+++ Assets/_Scripts/Flicker.cs
@@ -6,7 +6,8 @@ public class Flicker : MonoBehaviour {
 	public Light flashingLight;
 	// public Light secondFlashingLight;
 	private float randomNumber;
-	
+	public Material[] materials;
+	public GameObject sphere;
 	void Start(){
 		
 		flashingLight.enabled = false;
@@ -19,11 +20,12 @@ public class Flicker : MonoBehaviour {
 		randomNumber = Random.value;
 		
 		if (randomNumber <= 0.95f) {
-			
+
+			sphere.GetComponent<Renderer>().material = materials[0];
 			flashingLight.enabled = true;
 			// secondFlashingLight.enabled = true;
 		} else {
-			
+			sphere.GetComponent<Renderer>().material = materials[1];
 			flashingLight.enabled = false;
 			//  secondFlashingLight.enabled = false;
 			
