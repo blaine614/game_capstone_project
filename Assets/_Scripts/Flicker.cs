200ac9cfacd4156e9c8e364d1ff6e34e3f35859f
Aaron Ebbinghaus <aaronebb@gmail.com>
2015-10-18 21:43:40 -0400

    Lighting Prefabs and such

--- Assets/_Scripts/Flicker.cs
+++ Assets/_Scripts/Flicker.cs
@@ -6,8 +6,7 @@ public class Flicker : MonoBehaviour {
 	public Light flashingLight;
 	// public Light secondFlashingLight;
 	private float randomNumber;
-	public Material[] materials;
-	public GameObject sphere;
+	
 	void Start(){
 		
 		flashingLight.enabled = false;
@@ -20,12 +19,11 @@ public class Flicker : MonoBehaviour {
 		randomNumber = Random.value;
 		
 		if (randomNumber <= 0.95f) {
-
-			sphere.GetComponent<Renderer>().material = materials[0];
+			
 			flashingLight.enabled = true;
 			// secondFlashingLight.enabled = true;
 		} else {
-			sphere.GetComponent<Renderer>().material = materials[1];
+			
 			flashingLight.enabled = false;
 			//  secondFlashingLight.enabled = false;
 			
