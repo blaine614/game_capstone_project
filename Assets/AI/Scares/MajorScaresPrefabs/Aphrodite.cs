9ae3d5d9c03abcad0fd8c2681f4930d2fd9ea10d
finleyk23 <kbfinley@gmail.com>
2015-11-15 19:13:19 -0500

    AI

--- Assets/AI/Scares/MajorScaresPrefabs/Aphrodite.cs
+++ Assets/AI/Scares/MajorScaresPrefabs/Aphrodite.cs
@@ -12,7 +12,7 @@ public class Aphrodite : MonoBehaviour {
 	private bool glitchAdded;
 	private AudioSource sound1;
 	private AudioSource sound2;
-	private const float DEPTH = 2;
+	private const float DEPTH = 4;
 	private float glitchTimer = 4.0f;
 
 	// Use this for initialization
@@ -34,12 +34,17 @@ public class Aphrodite : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		const float ROTATION_SPEED = 10;
-		target = camera.transform.position - camera.transform.forward * DEPTH;
-		//check raycast
-		//RaycastHit rayHit;
-		//Physics.Raycast (camera.transform.position, camera.transform.forward, out rayHit); 
-		//if (Physics.Raycast(camera.ScreenPointToRay(camera.transform.forward), out rayHit))
-		//	beingWatched = true;
+		target = camera.transform.position - camera.transform.forward * 2;
+		target.y = transform.position.y;
+		//Debug.Log ("target: " + target);
+		RaycastHit hit = new RaycastHit();
+		Ray cameraRay = new Ray ();
+		cameraRay.direction = camera.transform.forward;
+		cameraRay.origin = camera.transform.position;
+		if (GetComponent<MeshCollider> ().Raycast (cameraRay, out hit, 10)) {
+			beingWatched = true;
+			camera.gameObject.GetComponent<GlitchEffect> ().enabled = true;
+		}
 		if (destroyObj && !sound2.isPlaying) {
 			Destroy (gameObject);
 		} else if (!beingWatched && !sound2.isPlaying) {
@@ -51,12 +56,17 @@ public class Aphrodite : MonoBehaviour {
 			lookPos.y = 0;
 			Quaternion quat = Quaternion.LookRotation (lookPos);
 			transform.rotation = Quaternion.Slerp (transform.rotation, quat, Time.deltaTime * ROTATION_SPEED);
-			agent.SetDestination (target);
-			agent.Resume ();
-			if ((agent.transform.position - target).magnitude < DEPTH) {
+			if ((transform.position - camera.transform.position).magnitude >= DEPTH) {
+				agent.SetDestination (target);
+				agent.Resume ();
+				sound1.Play();
+			}
+			else {//if ((agent.transform.position - target).magnitude < DEPTH) {
 				sound1.Pause ();
-			} else if (!sound1.isPlaying)
-				sound1.UnPause ();
+				agent.Stop();
+			}
+			//} else if (!sound1.isPlaying)
+			//	sound1.UnPause ();
 		} else if (!sound2.isPlaying) {
 			sound1.Pause ();
 			agent.Stop ();
@@ -67,6 +77,7 @@ public class Aphrodite : MonoBehaviour {
 			if (glitchTimer <= 0) {
 				transform.position = new Vector3(-100, -100, -100);
 				camera.gameObject.GetComponent<GlitchEffect>().intensity = 0.1f;
+				camera.gameObject.GetComponent<GlitchEffect> ().enabled = false;
 //				camera.gameObject.GetComponent<GlitchEffect>().glitchOn = false;
 			} else {
 				camera.gameObject.GetComponent<GlitchEffect>().intensity += 0.01f;
@@ -75,7 +86,7 @@ public class Aphrodite : MonoBehaviour {
 	}
 
 	void OnBecameVisible() {
-		beingWatched = true;
+		//beingWatched = true;
 		//camera.gameObject.GetComponent<GlitchEffect> ().glitchOn = true;
 	}
 }
