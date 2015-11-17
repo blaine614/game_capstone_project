9a95ed2d48fc7f6c92dd172cf811b488ad5af8d1
Trevor <rexsteak@yahoo.com>
2015-11-16 23:17:52 -0500

    Steve updates (vision, kind of)

--- Assets/_Scripts/SteveScript.cs
+++ Assets/_Scripts/SteveScript.cs
@@ -6,39 +6,77 @@ using System.Collections;
 public class SteveScript : MonoBehaviour {
 
     public int attackDistance = 2;
+
     private float distance;
+
     public GameObject[] waypoints;
-    private int waypointInd = 0;
+    private int waypointInd;
     private GameObject target;
+
     private NavMeshAgent agent;
-    private bool chasing = false;
+
     public float patrolSpeed = 1.5f;
     public float chaseSpeed = 2.5f;
+
     private Animator animator;
+
+    public State state;
+
+    private Vector3 investigateSpot;
+    private float invetigateTimer = 0;
+    public float investigateWait = 10;
+
+    public float heightMultiplier;
+    public float sightDist = 15;
+    public float lookSpeed = .03f;
+
+    private bool chasing = false;
+    private bool seePlayer = false;
+    private float seeTimer = 0;
+    private float seeWait = 3;
+
+    public enum State
+    {
+        PATROL,
+        CHASE,
+        INVESTIGATE
+    }
 
     void Start()
     {
-        target = waypoints[waypointInd];
+        
         agent = gameObject.GetComponent<NavMeshAgent>();
         animator = gameObject.GetComponent<Animator>();
+
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        waypointInd = Random.Range(0, waypoints.Length);
+
+        state = SteveScript.State.PATROL;
+        target = waypoints[waypointInd];
+
+        heightMultiplier = .6f;
     }
 
     void Update()
     {
-        if (!chasing)
+        switch (state)
         {
-            Patrolling();
+            case State.PATROL:
+                Patrolling();
+                break;
+            case State.CHASE:
+                Chasing();
+                break;
+            case State.INVESTIGATE:
+                Investigating();
+                break;
         }
-        else
-        {
-            Chasing();
-        }
-
 
     }
 
     void Patrolling()
     {
+        animator.SetBool("Looking", false);
         agent.speed = patrolSpeed;
         distance = Vector3.Distance(target.transform.position, transform.position);
 
@@ -48,24 +86,40 @@ public class SteveScript : MonoBehaviour {
         }
         else if (distance <= 2)
         {
-            waypointInd += 1;
-            if (waypointInd >= waypoints.Length)
-            {
-                waypointInd = 0;
-            }
+            waypointInd = Random.Range(0, waypoints.Length);
+
             target = waypoints[waypointInd];
         }
     }
 
     void Chasing()
     {
-        agent.speed = chaseSpeed;
-        distance = Vector3.Distance(target.transform.position, transform.position);
-        agent.SetDestination(target.transform.position);
-
-        if (distance <= attackDistance)
+        invetigateTimer = 0;
+        if (seePlayer)
         {
-            StartCoroutine(Attack()); 
+            seeTimer = 0;
+        }
+        else
+        {
+            seeTimer += Time.deltaTime;
+        }
+        if(seeTimer <= seeWait)
+        { 
+            chasing = true;
+            animator.SetBool("Looking", false);
+            agent.speed = chaseSpeed;
+            distance = Vector3.Distance(target.transform.position, transform.position);
+            agent.SetDestination(target.transform.position);
+
+            if (distance <= attackDistance)
+            {
+                StartCoroutine(Attack()); 
+            }
+        }
+        else
+        {
+            state = SteveScript.State.INVESTIGATE;
+            chasing = false;
         }
     }
 
@@ -73,17 +127,81 @@ public class SteveScript : MonoBehaviour {
     {
         animator.SetBool("Attack", true);
         yield return new WaitForSeconds(.75f);
-        chasing = false;
+        state = SteveScript.State.PATROL;
         target.GetComponentInChildren<SanityMeter>().Kill();
     }
 
-    void OnTriggerEnter(Collider coll)
+    void Investigating()
     {
-        if (coll.tag == "Player")
+        animator.SetBool("Looking", true);
+        invetigateTimer += Time.deltaTime;
+        agent.SetDestination(this.transform.position);
+        Quaternion rotate = Quaternion.LookRotation(investigateSpot - transform.position);
+        if(Quaternion.Angle(transform.rotation, rotate) > 15)
         {
-            target = coll.gameObject;
-            chasing = true;
+            animator.SetBool("Turn", true);
         }
+        else
+        {
+            animator.SetBool("Turn", false);
+        }
+        //Debug.Log(Quaternion.Angle(transform.rotation, rotate));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotate, lookSpeed);
+        if (invetigateTimer >= investigateWait)
+        {
+            state = SteveScript.State.PATROL;
+            target = waypoints[waypointInd];
+            invetigateTimer = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        seePlayer = false;
+        RaycastHit hit;
+        Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, transform.forward * sightDist, Color.green);
+        Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized * sightDist, Color.green);
+        Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized * sightDist, Color.green);
+        if(Physics.Raycast (transform.position + Vector3.up * heightMultiplier, transform.forward, out hit, sightDist))
+        {
+            if(hit.collider.gameObject.tag == "Player")
+            {
+                state = SteveScript.State.CHASE;
+                target = hit.collider.gameObject;
+                investigateSpot = hit.collider.gameObject.transform.position;
+                seePlayer = true;
+            }
+        }
+        if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized, out hit, sightDist))
+        {
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                state = SteveScript.State.CHASE;
+                target = hit.collider.gameObject;
+                investigateSpot = hit.collider.gameObject.transform.position;
+                seePlayer = true;
+            }
+        }
+        if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized, out hit, sightDist))
+        {
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                state = SteveScript.State.CHASE;
+                target = hit.collider.gameObject;
+                investigateSpot = hit.collider.gameObject.transform.position;
+                seePlayer = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    { 
+        if (coll.tag == "Player" && !chasing)
+        {
+            state = SteveScript.State.INVESTIGATE;
+            investigateSpot = coll.gameObject.transform.position;
+        }
+        
     }
 
 }
